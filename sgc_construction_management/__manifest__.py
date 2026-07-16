241a4ad59dc17611204b3fe4909b25fc97db93b3
contabo-sgc-server <deploy@contabo-sgc.local>
2026-07-16 17:40:01 +0200

    auto: sync 2026-07-16T15:40:01Z

--- sgc_construction_management/__manifest__.py
+++ sgc_construction_management/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'SGC Construction Management',
-    'version': '19.0.1.2.5',
+    'version': '19.0.1.2.6',
     'summary': 'Manage BOQ, WBS, Work Orders, Billing, Quality & more',
     'description': """
 SGC Construction Management
